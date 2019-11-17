cc5d6541a3910bbdd6f705f09054b0a30f935cbe
CharlyLaba <42964779+CharlyLaba@users.noreply.github.com>
2019-11-17 22:01:19 +0100

    Update NewDictionaryCode.java

--- NewDictionaryCode.java
+++ NewDictionaryCode.java
@@ -8,116 +8,113 @@ import java.util.*;
 public class NewDictionaryCode {
 
 
-    public static void sorting(List<String> array) throws IOException {
-        FileWriter writer = new FileWriter("dTest.txt");
-
+    public static void sortListandFileWriter(List<String> list) throws IOException {
+        FileWriter fileWriter = new FileWriter("dTest.txt");
         int k;
-        for (int j = 0; j < array.size(); j++) {
-            for (int i = 0; i < array.size() - 1; i++) {
+        for (int j = 0; j < list.size(); j++) {
+            for (int i = 0; i < list.size() - 1; i++) {
                 k = i + 1;
-                if (array.get(i).compareToIgnoreCase(array.get(k)) > 0) {
-                    swapWords(k, i, array);
+                if (list.get(i).compareToIgnoreCase(list.get(k)) > 0) {
+                    swapWords(k, i, list);
 
                 }
             }
         }
 
-        for (int i = 0; i < array.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
 
-            writer.write(array.get(i) + "\n");
+            fileWriter.write(list.get(i) + "\n");
 
         }
 
-        writer.close();
+        fileWriter.close();
     }
 
-    private static void swapWords(int i, int j, List<String> array) { //When the sorting method finds the right position of the word, swaps its position with the previous word.
+    private static void swapWords(int i, int j, List<String> list) { //When the sorting method finds the right position of the word, swaps its position with the previous word.
 
-        String temp = array.get(i);
-        array.set(i, array.get(j));
-        array.set(j, temp);
+        String wordToSwap = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, wordToSwap);
     }
 
-    public static void scanForWord(LinkedList<String> array, String s) { //Scan if a word is already in the list or not
+    public static void scanForWordAndFindPosition(LinkedList<String> list, String wordToLookFor) {
 
-        if(array.contains(StringUtils.decapitalize(s))){
-            s = StringUtils.decapitalize(s);
-            System.out.println("The word " + s + " is in the position " + array.indexOf(s) + " of the list." + "\n");
+        if(list.contains(StringUtils.decapitalize(wordToLookFor))){
 
-        }else if(array.contains(StringUtils.capitalize(s))){
-            s = StringUtils.capitalize(s);
-            System.out.println("The word " + s + " is in the position " + array.indexOf(s) + " of the list." + "\n");
+            wordToLookFor = StringUtils.decapitalize(wordToLookFor);
+            System.out.println("The word " + wordToLookFor + " is in the position " + list.indexOf(wordToLookFor) + " of the list." + "\n");
 
-        }else{
+        }
+
+        else if(list.contains(StringUtils.capitalize(wordToLookFor))){
+
+            wordToLookFor = StringUtils.capitalize(wordToLookFor);
+            System.out.println("The word " + wordToLookFor + " is in the position " + list.indexOf(wordToLookFor) + " of the list." + "\n");
+
+        }
+
+        else{
             System.out.println("This word is not inside this list." + "\n");
         }
-
-
     }
 
 
 
     public static void main(String[] args) throws IOException {
 
-        //Create access to the unsorted dictionary file
-        File f = new File("/Users/carlos/IdeaProjects/pair-programming/src/com/company/unsorteddict.txt");
+        File f = new File("/Users/carlos/IdeaProjects/pair-programming/src/com/company/test.txt");
 
-        //Scanner to scan the file
-        Scanner scan = new Scanner(f);
+        Scanner fileScanner = new Scanner(f);
+        Scanner userInputScanner = new Scanner(System.in);
 
-        //Scanner for the user inputs
-        Scanner scanner = new Scanner(System.in);
-
-        LinkedList<String> words = new LinkedList<String>();
+        LinkedList<String> listOfWords = new LinkedList<String>();
 
         String string = "";
-        while (scan.hasNext()) {
-            string = scan.nextLine();
-            words.add(string);
+        while (fileScanner.hasNext()) {
+            string = fileScanner.nextLine();
+            listOfWords.add(string);
         }
 
         //First we sort the list to then give the user some actions with the sorted list
-        sorting(words);
+        sortListandFileWriter(listOfWords);
 
-        //Here will be the user inputs available
         boolean flag = true;
         while (flag) {
             System.out.println("=======MAIN MENU=======");
-            System.out.println("* If you want to print the list sorted, type a." + "\n" +
-                    "* If you want to know if a word is already in the list and its position, type b"+ "\n" +
-                    "* If you want to exit, type e.");
+            System.out.println("* If you want to print the list sorted, type 'print'." + "\n" +
+                    "* If you want to know if a word is already in the list and its position, type 'word'."+ "\n" +
+                    "* If you want to exit, type 'exit'.");
 
-            String response = scanner.nextLine();
+            String userInput = userInputScanner.nextLine();
 
-            if (response.equalsIgnoreCase("a")) {
+            if (userInput.equalsIgnoreCase("print")) {
                 System.out.println("This is the list sorted: ");
 
-                for(int i=0; i<words.size();i++) {
-                    System.out.println(words.get(i));
+                for(int i=0; i<listOfWords.size();i++) {
+                    System.out.println(listOfWords.get(i));
 
                 }
                 System.out.println(" ");
             }
 
 
-            else if(response.equalsIgnoreCase("b")){
+            else if(userInput.equalsIgnoreCase("word")){
 
                 System.out.println("Input the word you want to look for: ");
-                String s = scanner.nextLine();
+                String wordTolookFor = userInputScanner.nextLine();
 
-                scanForWord(words, s);
+                scanForWordAndFindPosition(listOfWords, wordTolookFor);
 
             }
 
-            else if(response.equalsIgnoreCase("e")){
+            else if(userInput.equalsIgnoreCase("exit")){
                 System.out.println("Goodbye.");
                 flag=false;
             }
 
             else{
-                System.out.println("This argument doesn't exist, try 'a, b, or e'" + "\n");
+                System.out.println("This argument doesn't exist. Try: 'Print', 'Word', or 'Exit'. ");
             }
         }
     }
 }
-
